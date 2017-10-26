a2a8d12ddd8ca5b9acb6ab666f022dc773b99c67
rvpetkov <rvpetkov@gmail.com>
2017-10-26 10:25:09 +0300

    Adding a Loader script.
    The Loader will be responsible for loading the Manager scripts at the beginning of a scene.

---
+++ Assets/Scripts/Loader.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Loader : MonoBehaviour {
+
+    public GameManager gameManager;
+    public LevelManager levelManager;
+    
+	void Awake () {
+		if(GameManager.Instance == null)
+            Instanti
+	}
+}
